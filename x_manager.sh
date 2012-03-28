fa3ba72cf36c4d113d6dd19b27d21877585200e2
leonzhou <leon.zhou@xplusz.com>
2012-03-28 18:33:53 +0800

    Fix bug and enhance settings.py

--- x_manager.sh
+++ x_manager.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+initialize_folders()
+{
+    mkdir ${project_name}/${app_name}/views
+
+}
+
+
 initialize_files()
 {
     # pip freeze -r xplusz-requirements.txt > requirements.txt
     echo "==>Initializing requirements.txt ........"
     pip freeze > dev_requirements.txt
-    cp ${lib_path}/ini_data/requirements_basic.txt ${app_path}/requirements.txt
+    cp ${lib_path}/ini_data/requirements_basic.txt ${workspace_path}/requirements.txt
 
     # README
     echo "==>Initializing README.md"
@@ -13,57 +20,95 @@ initialize_files()
     # Procfile
     echo "==>Initializing Procfile"
     touch Procfile
-    echo "web: python ${app_module_name}/manage.py run_gunicorn -b \"0.0.0.0:\$PORT\" -w 3" >> Procfile
+    echo "web: python ${project_name}/manage.py run_gunicorn -b \"0.0.0.0:\$PORT\" -w 3" >> Procfile
 
     # DB config
-    cp ${lib_path}/ini_data/pgurl.py ${app_module_path}/pgurl.py
-    sed -ie "s/app_name/${app_name}/g" ${app_module_path}/pgurl.py
+    cp ${lib_path}/ini_data/pgurl.py ${project_path}/pgurl.py
+    sed -ie "s/app_name/${project_name}/g" ${project_path}/pgurl.py
 
     # Enhance setting.py
     ## Module
-    sed '/INSTALLED_APPS/ a\
-    \    "gunicorn",
-    ' ${app_module_name}/settings.py >> ${app_module_name}/settings_tmp.py
-    rm ${app_module_name}/settings.py
-    mv ${app_module_name}/settings_tmp.py ${app_module_name}/settings.py
-    #cp ${lib_path}/ini_data/settings.py ${app_module_path}/settings.py
+    sed -ie '/INSTALLED_APPS/ a\
+    \    "gunicorn",\
+    \    "south",
+    ' ${project_name}/settings.py
+    line=`sed -n '/INSTALLED_APPS/=' ${project_name}/settings.py`
+    let "start=$line+2"
+    sed -ie "$start a\\
+        \    \"${app_name}\",
+        " ${project_name}/settings.py
     ## DB config
-    #sed -ie "s/DATABASES = {\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n}/DATABASES = pgurl.get_db_settings()/g" ${app_module_path}/settings.py
+    line=`sed -n '/DATABASES/=' ${project_name}/settings.py`
+    let "start=$line+1"
+    let "end=$line+9"
+    sed -ie "${start},${end}d" ${project_name}/settings.py
+    sed -ie "s/.*DATABASES.*/DATABASES = pgurl.get_db_settings()/g" ${project_name}/settings.py
+    sed -ie '1 i\
+    \import pgurl, os
+    ' ${project_name}/settings.py
+    ## Debug module
+    sed -ie 's/DEBUG = True/ \
+\PROJECT_DIR = os.getcwd() \
+\DEBUG = os.environ.get("DEBUG_MODE", "true") == "true" \
+    /g' ${project_name}/settings.py
+    ## TimeZone
+    sed -ie 's/America.Chicago/UTC/g' ${project_name}/settings.py
+    ## Template
+    line=`sed -n '/TEMPLATE_DIRS/=' ${project_name}/settings.py`
+    let "start=$line+3"
+    view_dir="PROJECT_DIR + \"/${project_name}/${app_name}/views\""
+    sed -ie "$start a\\
+    \    ${view_dir}
+    " ${project_name}/settings.py
 
-    rm ${app_module_path}/*.pye
+
+    # Remove unused files
+    rm ${project_path}/*.pyc
+    rm ${project_path}/*.pye
+    rm ${workspace_path}/*.pyc
+    rm ${workspace_path}/*.pye
 
     # Local setup script
     echo "==>Locale environment setup"
-    cp ${lib_path}/ini_data/setup.sh ${app_path}/setup.sh
+    cp ${lib_path}/ini_data/.gitignore ${workspace_path}/.gitignore
+    cp ${lib_path}/ini_data/setup.sh ${workspace_path}/setup.sh
 }
 
 initialize_blank_project()
 {
-    echo "?Application name:"
+    echo "?Workspace name:"
+    read workspace_name
+    echo "?Project name:"
+    read project_name
+    echo "?App name:"
     read app_name
-    echo "?Application module name:"
-    read app_module_name
-    app_path="${PWD}/${app_name}"
-    app_module_path="${app_path}/${app_module_name}"
-    echo "App path=>${app_path}"
-    echo "App module path=>${app_module_path}"
+    workspace_path="${PWD}/${workspace_name}"
+    project_path="${workspace_path}/${project_name}"
+    app_path="${project_path}/${app_name}"
+    echo "Workspace path=>${workspace_path}"
+    echo "Project path=>${project_path}"
 
     ## Initialize Django-Heroku project
-    if [ -d ${app_path} ]; then
-        cd ${app_path}
+    if [ -d ${workspace_path} ]; then
+        cd ${workspace_path}
     else
-        mkdir ${app_path} && cd ${app_path}
+        mkdir ${workspace_path} && cd ${workspace_path}
     fi
     if [ ! -e .ve ]; then
         virtualenv .ve --distribute
     fi
     source .ve/bin/activate
     pip install Django==1.3.1 psycopg2
-    django-admin.py startproject ${app_module_name}
-    chmod +x ${app_module_path}/manage.py
+    django-admin.py startproject ${project_name}
+    chmod +x ${project_path}/manage.py
+    ${project_name}/manage.py startapp ${app_name}
+
+    # Initialize folders
+    initialize_folders
 
     # Initialize files
     initialize_files
+
 
     # Execute Local setup
     #./setup.sh
@@ -71,7 +116,7 @@ initialize_blank_project()
 
 echo "=======Welcome to Xplusz Manager bash======="
 lib_path="${PWD}/`dirname $0`"
-echo "Work path===>${PWD}"
+echo "Current path===>${PWD}"
 echo "Kit path==>${lib_path}"
 echo "==========Please make a choice========"
 echo "1: Initialize blank Django-Heroku project?"
